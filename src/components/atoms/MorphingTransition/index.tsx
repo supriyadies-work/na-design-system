a730ec58e6fd3fc8001040805f3b0e421fda1ad7
herujest <heruu.js@protonmail.com>
2026-02-08 17:15:57 +0700

    fix: update MorphingTransition prop types to allow null values for refs

--- src/components/atoms/MorphingTransition/index.tsx
+++ src/components/atoms/MorphingTransition/index.tsx
@@ -3,9 +3,9 @@
 import React from "react";
 
 interface MorphingTransitionProps {
-  wrapRef?: React.RefObject<HTMLDivElement>;
-  svgRef?: React.RefObject<SVGSVGElement>;
-  pathRef?: React.RefObject<SVGPathElement>;
+  wrapRef?: React.RefObject<HTMLDivElement | null>;
+  svgRef?: React.RefObject<SVGSVGElement | null>;
+  pathRef?: React.RefObject<SVGPathElement | null>;
   gradientId?: string;
   gradientColors?: {
     start: string;
